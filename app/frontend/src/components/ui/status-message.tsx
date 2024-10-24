cde49e301f531d6f11ce23260bb6115d43cbebaf
intrinsicvardhan <avardhan.das@gmail.com>
2024-10-24 11:13:30 +0530

    fixing ui

--- app/frontend/src/components/ui/status-message.tsx
+++ app/frontend/src/components/ui/status-message.tsx
@@ -6,8 +6,21 @@ type Properties = {
 
 export default function StatusMessage({ isRecording }: Properties) {
     if (!isRecording) {
-        return <p className="text mb-4 mt-6">Ask anything about Contoso employee benefits</p>;
-    }
+        return (
+          <div style={{ textAlign: 'center' }}>
+            <p className="text mb-4 mt-6">Ask me anything bank related service</p>
+            <p className="text mb-4 mt-6">बैंक से संबंधित सेवा के बारे में मुझसे कुछ भी पूछें </p>
+            <p className="text mb-4 mt-6">ব্যাংক সম্পর্কিত পরিষেবা সম্পর্কে আমাকে কিছু জিজ্ঞাসা করুন </p>
+            <p className="text mb-4 mt-6">வங்கி தொடர்பான சேவையைப் பற்றி என்னிடம் ஏதேனும் கேளுங்கள் </p>
+            <p className="text mb-4 mt-6">ബാങ്കുമായി ബന്ധപ്പെട്ട സേവനത്തെക്കുറിച്ച് എന്നോടു ഒന്നും ചോദ്യമിടാം </p>
+            <p className="text mb-4 mt-6">బ్యాంక్ సంబంధిత సేవ గురించి నన్ను ఏదైనా అడగండి </p>
+            <p className="text mb-4 mt-6">ಬ್ಯಾಂಕ್ ಸಂಬಂಧಿತ ಸೇವೆಯ ಬಗ್ಗೆ ನನ್ನನ್ನು ಏನಾದರೂ ಕೇಳಿ </p>
+            <p className="text mb-4 mt-6">બેંક સંબંધિત સેવા વિશે મને કંઈપણ પૂછો </p>
+            <p className="text mb-4 mt-6">ਬੈਂਕ ਨਾਲ ਸਬੰਧਤ ਸੇਵਾ ਬਾਰੇ ਮੈਨੂੰ ਕੁਝ ਵੀ ਪੁੱਛੋ </p>
+            <p className="text mb-4 mt-6">बँकेशी संबंधित सेवेबद्दल मला काहीही विचारा </p>
+          </div>
+        );
+      }
 
     return (
         <div className="flex items-center">
